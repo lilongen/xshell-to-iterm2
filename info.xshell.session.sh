5f036bd522d8b252cce6f49a9e9a74c8258809b3
lilongen <lilongen@gmail.com>
2016-07-26 17:53:17 +0800

    1

--- info.xshell.session.sh
+++ info.xshell.session.sh
@@ -3,4 +3,10 @@
 
 XshellSessionLocation=$1
 cd $XshellSessionLocation
-find  .  -name "*.xsh" -exec echo -n "{};" \; -exec grep -o -P "(?<=^Host=)([\w\-\.]+)" "{}" \; > /tmp/_xshell.sessions.info
+os=$(uname -a)
+
+if [ $os == "Linux"* ]; then
+	find  .  -name "*.xsh" -exec echo -n "{};" \; -exec grep -o -P "(?<=^Host=)([\w\-\.]+)" "{}" \; > /tmp/_xshell.sessions.info
+else 
+	find  .  -name "*.xsh" -exec echo -n "{};" \; -exec perl -nle 'print $1 if m{(?<=^Host=)([\w\.\-]+)}' "{}" \; > /tmp/_xshell.sessions.info
+fi

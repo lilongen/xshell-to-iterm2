0cfe251218c74d53b3cfebbd312b8c0d8490dfb8
lilongen <lilongen@gmail.com>
2016-07-26 17:56:28 +0800

    osx support

--- info.xshell.session.sh
+++ info.xshell.session.sh
@@ -5,7 +5,7 @@ XshellSessionLocation=$1
 cd $XshellSessionLocation
 os=$(uname -a)
 
-if [ $os == "Linux"* ]; then
+if [[ $os == "Linux"* ]]; then
 	find  .  -name "*.xsh" -exec echo -n "{};" \; -exec grep -o -P "(?<=^Host=)([\w\-\.]+)" "{}" \; > /tmp/_xshell.sessions.info
 else 
 	find  .  -name "*.xsh" -exec echo -n "{};" \; -exec perl -nle 'print $1 if m{(?<=^Host=)([\w\.\-]+)}' "{}" \; > /tmp/_xshell.sessions.info
